dfa19437a666897faeede7db7de3e1e374cd81bb
Sophia Jennifer Rose <sophia.j.rose@proton.me>
2024-12-04 09:40:31 +0000

    Day 4

---
+++ solutions/day4.ts
@@ -0,0 +1,79 @@
+export function partOne(input: string): string {
+    let grid: string[][] = [];
+    let lines: string[];
+    try {
+        lines = input.trim().split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            grid[i] = lines[i].split("");
+        }
+    } catch (err) {
+        console.log(err);
+        return "Error parsing input.";
+    }
+    try {
+        let count: number = 0;
+        const word: string = "XMAS";
+        const dirs: number[][] = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]];
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == "X") {
+                    for (let dir of dirs) {
+                        let wordIndex: number = 1;
+                        let newPos: number[] = [i, j];
+                        while (wordIndex < 4) {
+                            newPos = [newPos[0] + dir[0], newPos[1] + dir[1]];
+                            if (newPos[0] < 0 || newPos[0] >= grid.length || newPos[1] < 0 || newPos[1] >= grid[newPos[0]].length) {
+                                break;
+                            }
+                            if (grid[newPos[0]][newPos[1]] == word.charAt(wordIndex)) {
+                                wordIndex++;
+                            } else {
+                                break;
+                            }
+                        }
+                        if (wordIndex == 4) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        return String(count);
+    } catch (err) {
+        console.log(err);
+        return "Error when solving";
+    }
+}
+
+export function partTwo(input: string): string {
+    let grid: string[][] = [];
+    let lines: string[];
+    try {
+        lines = input.trim().split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            grid[i] = lines[i].split("");
+        }
+    } catch (err) {
+        console.log(err);
+        return "Error parsing input.";
+    }
+    try {
+        let count: number = 0;
+        // Finding X-MASes by finding A, which cannot be at edge of grid, so iterate for 1 to grid.length-2
+        for (let i = 1; i < grid.length-1; i++) {
+            for (let j = 1; j < grid[i].length-1; j++) {
+                if (grid[i][j] == "A") {
+                    let diagonals: string = [grid[i-1][j-1], grid[i-1][j+1], grid[i+1][j-1], grid[i+1][j+1]].toString();
+                    if (diagonals == "M,M,S,S" || diagonals == "S,M,S,M" || diagonals == "S,S,M,M" || diagonals == "M,S,M,S") {
+                        count++;
+                        console.log(`X-MAS at ${i}, ${j}`);
+                    }
+                }
+            }
+        }
+        return String(count);
+    } catch (err) {
+        console.log(err);
+        return "Error when solving";
+    }
+}
